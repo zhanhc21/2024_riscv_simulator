3494ae530381abdea2a496f03ab2a818d8e0e7a0
zhanhc <zhanhc21@mails.tsinghua.edu.cn>
2024-06-08 23:59:59 +0800

    step 2

--- test/cache_line_size.cpp
+++ test/cache_line_size.cpp
@@ -4,7 +4,7 @@
 constexpr unsigned ARRAY_SIZE = (1u << 13u);  // 8 kb
 char arr[ARRAY_SIZE];
 
-constexpr int totalRound = 8;
+constexpr int totalRound = 2;
 
 int main(int argc, char **argv) {
     auto testStep = ((unsigned) argv[0]);
@@ -13,13 +13,12 @@ int main(int argc, char **argv) {
     for (int i = 0; i < totalRound; i++) {
         unsigned index = 0;
         // 相同访问次数
-        for (unsigned j = 0; j < 256; ++j) {
+        for (unsigned j = 0; j < 2048; ++j) {
             sum += arr[index];
             index = (index + testStep) % ARRAY_SIZE;
         }
     }
 
     asm volatile(".word 0x0000000b");
-
     return sum;
 }
