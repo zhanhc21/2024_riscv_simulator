b28a4a862cccaa43cbe3eb4dba2f67f0766ffe98
zhanhc <zhanhc21@mails.tsinghua.edu.cn>
2024-06-11 14:11:31 +0800

    step 3

--- cache-exp/associativity.cpp
+++ cache-exp/associativity.cpp
@@ -13,7 +13,7 @@ unsigned MeasureCacheAssociativity([[maybe_unused]] ProcessorAbstract *p,
     unsigned testTime[6];
 
     for (int i = 0; i < 6; i++) {
-        testTime[i] = execute(p, "./test/block_size", cacheSize, associate[i]);
+        testTime[i] = execute(p, "./test/associate", cacheSize, associate[i]);
     }
 
     for (int i = 0; i < 6; i++) {
